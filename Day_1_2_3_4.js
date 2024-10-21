dd5864a3b1a9541eccede8b3bb3402b78f48ef36
KumarSunil_2024 <sunil.expertjavadeveloper@gmail.com>
2024-10-21 16:51:22 +0530

    Initial commit with JavaScript files

---
+++ Day_1_2_3_4.js
@@ -0,0 +1,10 @@
+console.log("Welcome to JavaScript");
+console.log(10+10);   //20 
+console.log(10+"10")  //1010
+console.log(100+ +"100");   //200
+console.log(10+"10"+10);   //101010
+console.log(10+ +"10"+10);  //30
+console.log(10-"2");   //8
+console.log(10-"2"-8);  //0
+console.log(10+"2"-"2");  //100
+console.log(true>8);
